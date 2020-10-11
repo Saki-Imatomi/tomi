9bc989c702fc6ae10bf483c4b0b377ffa3352ce6
saki <saki2283@outlook.com>
2020-10-11 19:08:07 +0900

    post kaeta

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
 
     def index
         @posts = Post.all
-        @all_ranks = Post.all.limit(3)
+        @all_ranks = Post.joins(:loves).group(:post_id).order('count(post_id) desc')
     end
 
 
