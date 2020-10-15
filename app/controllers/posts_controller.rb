495decfaab8114f58c8a00e05aaffb45d659969b
saki <saki2283@outlook.com>
2020-10-15 21:55:51 +0900

    post.controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
     def index
         @posts = Post.all
-        #@all_ranks = Post.find(Love.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
+        @all_ranks = Post.find(Love.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
     end
 
 
