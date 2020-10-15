3ab2bb27fc02465693093b193a73a9bf3296b8c8
saki <saki2283@outlook.com>
2020-10-15 21:25:28 +0900

    love

--- db/migrate/20200920112719_create_loves.rb
+++ db/migrate/20200920112719_create_loves.rb
@@ -1,6 +1,6 @@
 class CreateLoves < ActiveRecord::Migration[6.0]
   def change
-    create_table :lofe do |t|
+    create_table :loves do |t|
       t.references :post, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
