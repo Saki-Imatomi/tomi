2b775a21501c99e8e027f36b22ac090fc6163d22
saki <saki2283@outlook.com>
2020-10-15 20:34:44 +0900

    like.love.controller

--- app/controllers/loves_controller.rb
+++ app/controllers/loves_controller.rb
@@ -1,13 +1,13 @@
 class LovesController < ApplicationController
     def create
-        @love = current_user.loves.create(post_id: params[:post_id])
+        love = current_user.loves.create(post_id: params[:post_id])
         redirect_back(fallback_location: root_path)
     end
     
     
     def destroy
-        @love = Love.find_by(post_id: params[:post_id], user_id: current_user.id)
-        @love.destroy
+        love = Love.find_by(post_id: params[:post_id], user_id: current_user.id)
+        love.destroy
         redirect_back(fallback_location: root_path)
     end
 
